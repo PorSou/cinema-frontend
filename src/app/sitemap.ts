71080f1a2546b7b085c84e6920e48a04bd8f942b
PorSou <sinhporsurii@gmail.com>
2026-09-20 19:04:23 +0700

    Fix sitemap URLs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ export default function sitemap() {
   const base = "https://cinema-frontend-py8v.vercel.app";
   return [
     { url: base, lastModified: new Date() },
-    { url: `${base}/movies`, lastModified: new Date() },
-    { url: `${base}/cinemas`, lastModified: new Date() },
+    { url: `${base}/REAL-MOVIES-PATH`, lastModified: new Date() },
+    { url: `${base}/REAL-CINEMAS-PATH`, lastModified: new Date() },
   ];
 }
