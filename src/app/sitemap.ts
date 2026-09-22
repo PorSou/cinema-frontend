55843e76037ef6414f57805479cdc3287de9468a
PorSou <sinhporsurii@gmail.com>
2026-09-22 20:30:13 +0700

    push

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const base = "https://cinema-frontend-py8v.vercel.app";
+  const base = "https://www.porsou.store/";
   return [
     { url: base, lastModified: new Date() },
     { url: `${base}/REAL-MOVIES-PATH`, lastModified: new Date() },
